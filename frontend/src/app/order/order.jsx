64874243edf7b2ed00b6bc66dde6ffb368958700
Dmitriy <balalajkalodocnik@gmail.com>
2020-11-15 13:35:51 +0400

    dsa

--- frontend/src/app/order/order.jsx
+++ frontend/src/app/order/order.jsx
@@ -15,12 +15,14 @@ import {
 import PaymentPageSdk from '@raiffeisen-ecom/payment-sdk';
 import { useEffect, useState } from "react";
 import { WebApiUrl } from "../../config";
+import Auth from "../global/auth/auth";
 
 const Order = (props) => {
     const url = WebApiUrl;
 
     const payButtonColour = props.styles.payButtonColour ? props.styles.payButtonColour : "primary";
 
+    const [authIsOpen, setAuthIsOpen] = useState(false);
     const [isChecked, setIsChecked] = useState([]);
 
     const [price, setPrice] = useState(0);
@@ -49,7 +51,7 @@ const Order = (props) => {
                 setUnpaidOrder(unpaidOrderJson);
                 console.log("load => unpaidOrderJson", unpaidOrderJson);
 
-                // await getPaymentInfo();
+                await getPaymentInfo();
 
                 let checked = unpaidOrderJson.orders.map(x => {
                     return { orderId: x.id, isCheck: false }
@@ -72,12 +74,14 @@ const Order = (props) => {
         let unpaidResponse = await fetch(url + "/api/order/list/unpaid");
         let unpaid = await unpaidResponse.json();
 
+        await getPaymentInfo();
+
         if (!unpaid.length) {
             const paymentPage = new PaymentPageSdk('000001680200002-80200002', {
                 url: 'https://test.ecom.raiffeisen.ru/pay'
             });
 
-            paymentPage.openWindow({ amount: 10.10 });
+            paymentPage.openWindow({ amount: price });
         }
     }
 
@@ -91,7 +95,15 @@ const Order = (props) => {
         }
     }
 
-    const savePaymentInfo = () => {
+    const savePaymentInfo = async () => {
+        let isAuthResp = await fetch(url + "/api/user/is-authenticated");
+        let isAuthJson = await isAuthResp.json();
+
+        if (!isAuthJson) {
+            setAuthIsOpen(true);
+            return;
+        }
+
         let ordersId = isChecked.filter(x => x.isCheck).map(x => x.orderId);
 
         fetch(url + "/api/payment-info/new", {
@@ -104,73 +116,76 @@ const Order = (props) => {
     }
 
     return (
-        <Container className="mt-3">
-            <Row>
-                {/* Список заказов */}
-                <Col lg='8' className="align-self-start">
-                    <Card>
-                        <CardHeader>
-                            <h3>Ваш заказ</h3>
-                        </CardHeader>
-                        <CardBody>
-                            <CardText>Выберите те позиции, которые вы хотите оплатить</CardText>
-                            <CardText>Если одну позицию выберет несколько человек,
-                            то цена за эту позицию будет разделена между этими людьми</CardText>
-                        </CardBody>
-                    </Card>
-                    <br />
-                    {unpaidOrder.orders.length ? <>
+        <>
+            <Container className="mt-3">
+                <Row>
+                    {/* Список заказов */}
+                    <Col lg='8' className="align-self-start">
                         <Card>
+                            <CardHeader>
+                                <h3>Ваш заказ</h3>
+                            </CardHeader>
                             <CardBody>
-                                <Button color="primary" onClick={savePaymentInfo}>Сохранить</Button>
-                                <Button className="ml-3" color="primary" onClick={getPaymentInfo}>Обновить</Button>
+                                <CardText>Выберите те позиции, которые вы хотите оплатить</CardText>
+                                <CardText>Если одну позицию выберет несколько человек,
+                            то цена за эту позицию будет разделена между этими людьми</CardText>
                             </CardBody>
                         </Card>
                         <br />
-                        <Card>
-                            <CardHeader>
-                                <h3>Неоплаченные позиции</h3>
-                            </CardHeader>
-                            <CardBody>
-                                {unpaidOrder.orders.map((item, index) =>
-                                    <InputGroup key={index}>
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <Input addon
-                                                    type="checkbox"
-                                                    checked={isChecked.filter(x => x.orderId == item.id).isCheck}
-                                                    onChange={() => onCheck(item.id)}
-                                                />
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input disabled placeholder={`${item.name} | ${item.price} р`} />
-                                    </InputGroup>
-                                )}
-                            </CardBody>
-                        </Card>
-                    </> : <Card body>
-                            <CardText>Сейчас тут ничего нет, так как вы ничего не заказали</CardText>
-                        </Card>}
+                        {unpaidOrder.orders.length ? <>
+                            <Card>
+                                <CardBody>
+                                    <Button color="primary" onClick={savePaymentInfo}>Сохранить</Button>
+                                    <Button className="ml-3" color="primary" onClick={getPaymentInfo}>Обновить</Button>
+                                </CardBody>
+                            </Card>
+                            <br />
+                            <Card>
+                                <CardHeader>
+                                    <h3>Неоплаченные позиции</h3>
+                                </CardHeader>
+                                <CardBody>
+                                    {unpaidOrder.orders.map((item, index) =>
+                                        <InputGroup key={index}>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <Input addon
+                                                        type="checkbox"
+                                                        checked={isChecked.filter(x => x.orderId == item.id).isCheck}
+                                                        onChange={() => onCheck(item.id)}
+                                                    />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input disabled placeholder={`${item.name} | ${item.user.nickname} | ${item.price}  р`} />
+                                        </InputGroup>
+                                    )}
+                                </CardBody>
+                            </Card>
+                        </> : <Card body>
+                                <CardText>Сейчас тут ничего нет, так как вы ничего не заказали</CardText>
+                            </Card>}
 
-                </Col>
-                {/* Оплатить */}
-                <Col lg='4' className="align-self-start">
-                    {totalOrder.length || unpaidOrder.totalPrice || price ?
-                        <Card body>
-                            {unpaidOrder.totalPrice ?
-                                <h5>Осталось к оплате: {unpaidOrder.totalPrice}</h5>
-                                : ""}
-                            {price ?
-                                <>
-                                    <h5>Сумма к оплате: {price}</h5>
-                                    <Button color={payButtonColour} onClick={pay}>Оплатить</Button>
-                                </>
-                                : ""}
-                        </Card>
-                        : ""}
-                </Col>
-            </Row>
-        </Container >
+                    </Col>
+                    {/* Оплатить */}
+                    <Col lg='4' className="align-self-start">
+                        {totalOrder.length || unpaidOrder.totalPrice || price ?
+                            <Card body>
+                                {unpaidOrder.totalPrice ?
+                                    <h5>Осталось к оплате: {unpaidOrder.totalPrice}</h5>
+                                    : ""}
+                                {price ?
+                                    <>
+                                        <h5>Сумма к оплате: {price}</h5>
+                                        <Button color={payButtonColour} onClick={pay}>Оплатить</Button>
+                                    </>
+                                    : ""}
+                            </Card>
+                            : ""}
+                    </Col>
+                </Row>
+            </Container >
+            <Auth isOpen={authIsOpen} setIsOpen={setAuthIsOpen} />
+        </>
     )
 }
 
